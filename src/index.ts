6d58309ba8e9ca1c88249d3f5bbd96df25f5b9cd
tompng <tomoyapenguin@gmail.com>
2021-07-08 21:39:45 +0900

    hittest, random impact on collision

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,12 @@ class Vector3 {
       a * va.z + b * vb.z
     )
   }
+  static add(va: Vector3, vb: Vector3) {
+    return this.wsum(1, va, 1, vb)
+  }
+  static sub(va: Vector3, vb: Vector3) {
+    return this.wsum(1, va, -1, vb)
+  }
 }
 
 function vectorMinus(v: Vector3, n: number) {
@@ -138,8 +144,8 @@ class Cube {
   }
 
   update(dt: number) {
-    this.velocity = Vector3.wsum(1, this.velocity, dt, Gravity)
-    this.position = Vector3.wsum(1, this.position, dt , this.velocity)
+    this.velocity = Vector3.add(this.velocity, Gravity.scale(dt))
+    this.position = Vector3.add(this.position, this.velocity.scale(dt))
     const w = Matrix3.fromRotation(this.momentum, this.momentum.length() * dt)
     this.rotation = w.mult(this.rotation)
     this.hitFloor()
@@ -152,11 +158,11 @@ class Cube {
   ]
   hitFloor() {
     const floorZ = -4
-    const faceDown = Cube.faces.some(n => this.rotation.transform(n).z > 0.99)
+    const faceDown = Cube.faces.some(n => this.rotation.transform(n).z > 0.999)
 
     Cube.coords.forEach(coord => {
       const rpos = this.rotation.transform(coord).scale(this.size)
-      const point = Vector3.wsum(1, this.position, 1, rpos)
+      const point = Vector3.add(this.position, rpos)
       if (point.z > floorZ) return
       const h = floorZ - point.z
       this.position.z += h
@@ -171,27 +177,27 @@ class Cube {
         const mscale = Math.sqrt(Math.max(1 - 2 * wLossEnergy / this.momentum.length() ** 2, 0))
         if (mscale < 1) this.momentum = this.momentum.scale(mscale)
       }
-      const vel = Vector3.wsum(1, this.velocity, 1, Vector3.cross(this.momentum, rpos))
+      const vel = Vector3.add(this.velocity, Vector3.cross(this.momentum, rpos))
       if (vel.z > 0) return
       const rxy = Math.hypot(vel.x, vel.y) || 1
       const friction = 0.5
       const Fz = new Vector3(0, 0, -vel.z)
       const Fxy = new Vector3(friction * vel.z * vel.x / rxy, friction * vel.z * vel.y / rxy, 0)
       const v0 = vel
-      const v1 = Vector3.wsum(1, Fz, 1, Vector3.cross(Vector3.cross(rpos, Fz), rpos))
+      const v1 = Vector3.add(Fz, Vector3.cross(Vector3.cross(rpos, Fz), rpos))
       // velocity = v0 + v1 * t
       // position = v0.z * t + v1.z * t * t / 2
       const t = - 1.5 * v0.z / v1.z
       if (isNaN(t) || t <= 0) return
-      const a = Vector3.wsum(1, this.velocity, t, Fz)
+      const a = Vector3.add(this.velocity, Fz.scale(t))
       const af = Fxy.scale(t)
-      const b = Vector3.wsum(1, this.momentum, t, Vector3.cross(rpos, Fz))
+      const b = Vector3.add(this.momentum, Vector3.cross(rpos, Fz).scale(t))
       const bf = Vector3.cross(rpos, Fxy).scale(t)
       const _f = - (Vector3.dot(a, af) + Vector3.dot(b, bf)) / (Vector3.dot(af, af) + Vector3.dot(bf, bf))
       const f = _f < 0 ? 0 : _f < 1 ? _f : 1
-      const F = Vector3.wsum(1, Fz, f, Fxy)
-      this.velocity = Vector3.wsum(1, this.velocity, t, F)
-      this.momentum = Vector3.wsum(1, this.momentum, t, Vector3.cross(rpos, F))
+      const F = Vector3.add(Fz, Fxy.scale(f))
+      this.velocity = Vector3.add(this.velocity, F.scale(t))
+      this.momentum = Vector3.add(this.momentum, Vector3.cross(rpos, F).scale(t))
     })
   }
 
@@ -202,9 +208,9 @@ class Cube {
       Cube.coords.forEach(q => {
         if (Vector3.distance(p, q) != 2) return
         const [tp, tq] = [p, q].map(
-          point => Vector3.wsum(1, this.position, this.size, this.rotation.transform(point))
+          point => Vector3.add(this.position, this.rotation.transform(point).scale(this.size))
         )
-        const center = this.rotation.transform(Vector3.wsum(0.5, p, 0.5, q))
+        const center = this.rotation.transform(Vector3.add(p, q).scale(0.5))
         const ps = 1 / (1 + 0.1 * tp.y)
         const qs = 1 / (1 + 0.1 * tq.y)
         ctx.globalAlpha = 0.6 - 0.3 * center.y
@@ -216,6 +222,30 @@ class Cube {
     })
     ctx.restore()
   }
+
+  static hitTest(cube1: Cube, cube2: Cube) {
+    for (const [a, b] of [[cube1, cube2], [cube2, cube1]]) {
+      const faces = [new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1)].map(f => a.rotation.transform(f))
+      const coords = Cube.coords.map(c => Vector3.add(b.rotation.transform(c).scale(b.size), Vector3.sub(b.position, a.position)))
+      for (const f of faces) {
+        if (coords.every(c => a.size < Vector3.dot(f, c)) || coords.every(c => Vector3.dot(f, c) < -a.size)) return false
+      }
+    }
+    return true
+  }
+  static hit(a: Cube, b: Cube) {
+    const diff = Vector3.sub(a.position, b.position)
+    if (this.hitTest(a, b)) {
+      const dir = diff.normalize()
+      const dot = Vector3.dot(Vector3.sub(a.velocity, b.velocity), dir)
+      if (dot < 0) {
+        a.velocity = Vector3.add(a.velocity, dir.scale(-dot))
+        b.velocity = Vector3.add(b.velocity, dir.scale(dot))
+        a.momentum = Vector3.add(a.momentum, randomDirection())
+        b.momentum = Vector3.add(b.momentum, randomDirection())
+      }
+    }
+  }
 }
 
 function assignGlobal(data: Record<string, any>) {
@@ -226,21 +256,29 @@ function assignGlobal(data: Record<string, any>) {
 
 assignGlobal({ Matrix3, Vector3, Cube })
 
-const cube = new Cube(new Vector3(0, 0, 4))
+const cube = new Cube(new Vector3(0, 0, 6))
+const cube2 = new Cube(new Vector3(0, 0, 2))
 const ctx = canvas.getContext('2d')!
 setInterval(() => {
-  cube.update(0.1)
+  Cube.hit(cube, cube2)
   ctx.clearRect(0, 0, SIZE, SIZE)
-  ctx.save()
-  ctx.translate(SIZE / 2, SIZE / 2)
-  ctx.lineWidth = 0.02
-  ctx.scale(SIZE / 16, SIZE / 16)
-  cube.render(ctx)
-  ctx.restore()
+  ;[cube, cube2].forEach(c => {
+    c.update(0.1)
+    ctx.save()
+    ctx.translate(SIZE / 2, SIZE / 2)
+    ctx.lineWidth = 0.02
+    ctx.scale(SIZE / 16, SIZE / 16)
+    c.render(ctx)
+    ctx.restore()
+  })
 }, 10)
 
 assignGlobal({ cube })
 document.onclick = () => {
-  cube.position = new Vector3(0, 0, 4)
+  cube.position = new Vector3(0, 0, 6)
   cube.momentum = randomDirection(4)
+  cube2.position = new Vector3(0, 0, 2)
+  cube2.momentum = randomDirection(4)
+  cube.velocity = new Vector3(0, 0, 0)
+  cube2.velocity = new Vector3(0, 0, 0)
 }
